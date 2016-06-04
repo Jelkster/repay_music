84c23442734f761b103e4e9348a15424d62218e3
Ryan Jelks <jelkster@gmail.com>
2016-06-04 03:16:44 -0400

    app helper specs, remove old test dir

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the ApplicationHelper. For example:
+#
+# describe ApplicationHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe ApplicationHelper, type: :helper do
+  describe '#currency' do
+    it 'shows cents if present' do
+      expect(helper.currency(6666.66)).to eq('$6,666.66')
+    end
+
+    it 'does not show cents unless present' do
+      expect(helper.currency(6666)).to eq('$6,666')
+    end
+  end
+end
